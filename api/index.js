f33624e9a3379eccc4f4bbb7d2322ad6e1727015
Ivyn San Pedro <isanpedro@gmail.com>
2019-04-02 22:26:46 -0400

    Read data from MongoDB

--- api/index.js
+++ api/index.js
@@ -1,23 +1,37 @@
 import express from 'express';
-import data from '../src/testData';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+let mdb;
+MongoClient.connect(config.mongodbUri, { useNewUrlParser: true }, (err, db) => {
+  assert.equal(null, err);
+  mdb = db;
+});
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-  obj[contest.id] = contest;
-  return obj;
-}, {});
 
 router.get('/contests', (req, res) => {
-  res.send({ 
-    contests: contests
-  });
+  let contests = {};
+  mdb.db('test').collection('contests').find({})
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1
+    })
+    .forEach(contest => {
+      contests[contest.id] = contest;
+    }, err => {
+      assert.equal(null, err);
+      res.send({ contests });
+    });
 });
 
 router.get('/contests/:contestId', (req, res) => {
-  let contest = contests[req.params.contestId];
-  contest.description = 'lorem ipsum';
-
-  res.send(contest);
+  mdb.db('test').collection('contests')
+    .findOne({ id: Number(req.params.contestId) })
+    .then(contest => res.send(contest))
+    .catch(console.error);
 });
 
 export default router;
