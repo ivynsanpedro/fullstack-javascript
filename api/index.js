a04b551198dfdc6bdeaf79a19a1112c30ac893e9
Ivyn San Pedro <isanpedro@gmail.com>
2019-04-14 10:24:07 -0400

    Create an API to propose a name

--- api/index.js
+++ api/index.js
@@ -6,14 +6,14 @@ import config from '../config';
 let mdb;
 MongoClient.connect(config.mongodbUri, { useNewUrlParser: true }, (err, db) => {
   assert.equal(null, err);
-  mdb = db;
+  mdb = db.db('test');
 });
 
 const router = express.Router();
 
 router.get('/contests', (req, res) => {
   let contests = {};
-  mdb.db('test').collection('contests').find({})
+  mdb.collection('contests').find({})
     .project({
       categoryName: 1,
       contestName: 1
@@ -29,7 +29,7 @@ router.get('/contests', (req, res) => {
 router.get('/names/:nameIds', (req, res) => {
   const nameIds = req.params.nameIds.split(',').map(ObjectID);
   let names = {};
-  mdb.db('test').collection('names').find({ _id: { $in: nameIds }})
+  mdb.collection('names').find({ _id: { $in: nameIds }})
     .forEach(name => {
       names[name._id] = name;
     }, err => {
@@ -39,10 +39,36 @@ router.get('/names/:nameIds', (req, res) => {
 });
 
 router.get('/contests/:contestId', (req, res) => {
-  mdb.db('test').collection('contests')
+  mdb.collection('contests')
     .findOne({ _id: ObjectID(req.params.contestId) })
     .then(contest => res.send(contest))
-    .catch(console.error);
+    .catch(error => {
+      console.error(error);
+      res.status(404).send('Bad Request');
+    });
+});
+
+router.post('/names', (req, res) => {
+  const contestId = ObjectID(req.body.contestId);
+  const name = req.body.newName;
+  // validdation ...
+  mdb.collection('names').insertOne({ name }).then(result =>
+    mdb.collection('contests').findAndModify(
+      { _id: contestId },
+      [],
+      { $push: { nameIds: result.insertedId } },
+      { new: true}
+    ).then(doc =>
+      res.send({
+        updatedContest: doc.value,
+        newName: { _id: result.insertedId, name }
+      })
+    )
+  )
+  .catch(error => {
+    console.error(error);
+    res.status(404).send('Bad Request');
+  });
 });
 
 export default router;
